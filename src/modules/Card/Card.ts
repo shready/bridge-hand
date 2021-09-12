a974348bfc14f4f1c47e4484c4eaf70702ea37f5
Shaun Ready <readysd@gmail.com>
2021-09-12 15:50:20 -0300

    Create Card class with suit and face

---
+++ src/modules/Card/Card.ts
@@ -0,0 +1,21 @@
+import { Face } from './Face';
+import { Suit } from './Suit';
+
+export class Card {
+  private face: Face;
+
+  private suit: Suit;
+
+  constructor(face: Face, suit: Suit) {
+    this.face = face;
+    this.suit = suit;
+  }
+
+  getFace(): Face {
+    return this.face;
+  }
+
+  getSuit(): Suit {
+    return this.suit;
+  }
+}
---
+++ src/modules/Card/Suit.ts
@@ -0,0 +1,6 @@
+export enum Suit {
+  club = '♣',
+  diamond = '♦',
+  heart = '♥',
+  spade = '♠',
+}
